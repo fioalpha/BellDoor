4b8d32cec8b2fdb654edf8b3ea4d531ef82a5bb3
fioalpha <fioalpha@gmail.com>
2021-03-06 18:00:23 -0300

    add mqtt client

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,58 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include "reader/Reader.h"
+#include "secrets/secrets.h"
 
-#define RST_PIN D0      
-#define SS_PIN D8       
+#include <ESP8266WiFi.h> // Importa a Biblioteca ESP8266WiFi
+#include <PubSubClient.h> // Importa a Biblioteca PubSubClient
 
-Reader reader(SS_PIN, RST_PIN);
+WiFiClient wifiClient;
+
+PubSubClient MQTT(wifiClient);
+
+Reader reader(D8, D0);
+
+void mqttCallback(char* topic, byte* paylod, unsigned int length) {
+
+}
+
+void initMqttClient() {
+	MQTT.setServer(MQTT_BROKER, MQTT_PORT);
+	MQTT.setCallback(mqttCallback);
+}
+
+void mqttConnect() {
+	if (MQTT.connected()) return;
+
+	Serial.println("Starting mqtt client");
+	Serial.println("Trying connect the mqtt client");
+
+	while (!MQTT.connect(MQTT_CLIENT_ID, MQTT_USER, MQTT_PASS))
+		Serial.print('.');
+	
+	Serial.println("MQTT client connected");
+}
+
+void wifiConnect() {
+	if (WiFi.isConnected()) return ;
+	WiFi.begin(SSID_WIFI, PASS_WIFI);
+	Serial.println("Trying connectthe wifi");
+	while (!WiFi.isConnected()){
+		Serial.print(".");
+		delay(100);
+	}
+
+	Serial.println();
+	Serial.println("Connected on the wifi");
+	Serial.print(SSID_WIFI);
+    Serial.println("IP obtido: ");
+    Serial.println(WiFi.localIP());
+}
+
+void connectService() {
+	wifiConnect();
+	mqttConnect();
+}
 
 void setup() {
 	Serial.begin(9600);
@@ -13,12 +60,14 @@ void setup() {
 	SPI.begin();
 	reader.init();
 	delay(4);
+	initMqttClient();
 }
 
 void loop() {
+	connectService();
 	String tagRead = reader.read();
-	if(tagRead != "") {
+	if(!tagRead.equals("")) {
 		Serial.println(tagRead);
+		
 	}
-	delay(2000);
 }
